c1ab21639ac5ac4baf3f5e4c3035d499623baa0a
Tom Bell <tomb@tomb.io>
2022-09-22 23:35:48 +0100

    Update to not embed Cmd struct in Tmux struct

--- internal/tmux/tmux.go
+++ internal/tmux/tmux.go
@@ -21,24 +21,24 @@ type TmuxWindow struct {
 }
 
 type Tmux struct {
-	cmd.Cmd
+	cmd cmd.Cmd
 }
 
 func (t Tmux) NewSession(name, root, windowName string) (string, error) {
 	cmd := exec.Command("tmux", "new-session", "-Pd", "-s", name, "-n", windowName, "-c", root)
-	return t.Exec(cmd)
+	return t.cmd.Exec(cmd)
 }
 
 func (t Tmux) KillSession(target string) (string, error) {
 	cmd := exec.Command("tmux", "kill-session", "-t", target)
-	return t.Exec(cmd)
+	return t.cmd.Exec(cmd)
 }
 
 func (t Tmux) ListSessions() ([]TmuxSession, error) {
 	var sessions []TmuxSession
 
 	cmd := exec.Command("tmux", "list-sessions", "-F", "#{session_id};#{session_name};#{session_path}")
-	out, err := t.Exec(cmd)
+	out, err := t.cmd.Exec(cmd)
 	if err != nil {
 		return sessions, err
 	}
@@ -61,19 +61,19 @@ func (t Tmux) ListSessions() ([]TmuxSession, error) {
 
 func (t Tmux) NewWindow(target, name, root string) (string, error) {
 	cmd := exec.Command("tmux", "new-window", "-Pd", "-t", target, "-n", name, "-c", root, "-F", "#{window_id}")
-	return t.Exec(cmd)
+	return t.cmd.Exec(cmd)
 }
 
 func (t Tmux) KillWindow(target string) (string, error) {
 	cmd := exec.Command("tmux", "kill-window", "-t", target)
-	return t.Exec(cmd)
+	return t.cmd.Exec(cmd)
 }
 
 func (t Tmux) ListWindows(target string) ([]TmuxWindow, error) {
 	var windows []TmuxWindow
 
 	cmd := exec.Command("tmux", "list-windows", "-t", target, "-F", "#{window_id};#{window_name};#{pane_current_path};#{window_layout}")
-	out, err := t.Exec(cmd)
+	out, err := t.cmd.Exec(cmd)
 	if err != nil {
 		return windows, err
 	}
