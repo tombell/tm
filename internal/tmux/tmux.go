1e1a6c0e7ef925d13ba43aea898aac4e65eaf38c
Tom Bell <tomb@tomb.io>
2022-09-22 21:30:31 +0100

    Add window methods to tmux package

--- internal/tmux/tmux.go
+++ internal/tmux/tmux.go
@@ -13,6 +13,13 @@ type TmuxSession struct {
 	Root string
 }
 
+type TmuxWindow struct {
+	ID     string
+	Name   string
+	Root   string
+	Layout string
+}
+
 type Tmux struct {
 	cmd.Cmd
 }
@@ -50,4 +57,40 @@ func (t Tmux) ListSessions() ([]TmuxSession, error) {
 	}
 
 	return sessions, nil
+}
+
+func (t Tmux) NewWindow(target, name, root string) (string, error) {
+	cmd := exec.Command("tmux", "new-window", "-Pd", "-t", target, "-n", name, "-c", root, "-F", "#{window_id}")
+	return t.Exec(cmd)
+}
+
+func (t Tmux) KillWindow(target string) (string, error) {
+	cmd := exec.Command("tmux", "kill-window", "-t", target)
+	return t.Exec(cmd)
+}
+
+func (t Tmux) ListWindows(target string) ([]TmuxWindow, error) {
+	var windows []TmuxWindow
+
+	cmd := exec.Command("tmux", "list-windows", "-t", target, "-F", "#{window_id};#{window_name};#{pane_current_path};#{window_layout}")
+	out, err := t.Exec(cmd)
+	if err != nil {
+		return windows, err
+	}
+
+	windowList := strings.Split(out, "\n")
+
+	for _, w := range windowList {
+		windowInfo := strings.Split(w, ";")
+		window := TmuxWindow{
+			ID:     windowInfo[0],
+			Name:   windowInfo[1],
+			Root:   windowInfo[2],
+			Layout: windowInfo[3],
+		}
+
+		windows = append(windows, window)
+	}
+
+	return windows, nil
 }
