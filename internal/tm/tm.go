85a2f5491597ecb211a52f94059f4f37d4c7ad03
Tom Bell <tomb@tomb.io>
2022-09-26 22:35:14 +0100

    Update Start method to resolve roots for sessions and windows

--- internal/tm/tm.go
+++ internal/tm/tm.go
@@ -1,6 +1,10 @@
 package tm
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/tombell/tm/internal/cmd"
 	"github.com/tombell/tm/internal/config"
 	"github.com/tombell/tm/internal/tmux"
@@ -19,14 +23,22 @@ func New(tmux tmux.Tmux, cmd cmd.Cmd) Tm {
 	return Tm{tmux, cmd}
 }
 
-func (tm Tm) Start(cfg *config.Config) error {
+func (tm Tm) Start(cfg *config.Config, ctx Context) error {
+	root := expandPath(cfg.Root)
+
 	for _, s := range cfg.Sessions {
-		if _, err := tm.tmux.NewSession(s.Name, s.Root, defaultWindowName); err != nil {
+		if tm.tmux.SessionExists(s.Name) {
+			continue
+		}
+
+		sessionRoot := resolvePath(root, s.Root)
+		if _, err := tm.tmux.NewSession(s.Name, sessionRoot, defaultWindowName); err != nil {
 			return err
 		}
 
 		for _, w := range s.Windows {
-			if _, err := tm.tmux.NewWindow(s.Name, w.Name, "."); err != nil {
+			windowRoot := resolvePath(sessionRoot, w.Root)
+			if _, err := tm.tmux.NewWindow(s.Name, w.Name, windowRoot); err != nil {
 				return err
 			}
 		}
@@ -47,4 +59,25 @@ func (tm Tm) Stop(cfg *config.Config) error {
 	}
 
 	return nil
+}
+
+func expandPath(name string) string {
+	if strings.HasPrefix(name, "~/") {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return name
+		}
+
+		return strings.Replace(name, "~", homeDir, 1)
+	}
+
+	return name
+}
+
+func resolvePath(root, name string) string {
+	baseRoot := expandPath(name)
+	if baseRoot == "" || !filepath.IsAbs(baseRoot) {
+		baseRoot = filepath.Join(root, name)
+	}
+	return baseRoot
 }
